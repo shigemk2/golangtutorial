021111916373bd9c458f17db4f03e198762817bf
shigemk2 <i.am.shige@gmail.com>
2014-09-09 22:26:01 +0900

    Add 24

--- 24.go
+++ 24.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func Sqrt(x float64) float64 {
-	return x
+func Sqrt(z float64) float64 {
+	v := float64(0)
+	fmt.Println(math.Sqrt(z))
+	fmt.Println("-----------------------")
+	for i := float64(0); i < 10; i++ {
+		v = z - ((z*z - i) / (2 * z))
+		fmt.Println(v)
+	}
+	fmt.Println("-----------------------")
+	return v
 }
 
 func main() {
