28c9dbe63dcf986b9a9292a01f944e95a59e7413
shigemk2 <i.am.shige@gmail.com>
2014-09-10 00:18:36 +0900

    Add 30

---
+++ 30.go
@@ -0,0 +1,13 @@
+//  sliceは、値の配列を参照し、長さ( length )も含まれています。[]T は、 T 型の要素をもつsliceです。
+package main
+
+import "fmt"
+
+func main() {
+	p := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println("p ==", p)
+
+	for i := 0; i < len(p); i++ {
+		fmt.Printf("p[%d] == %d\n", i, p[i])
+	}
+}
