d1bd18c833d9ce9112b2f9222a696fefab982794
shigemk2 <i.am.shige@gmail.com>
2014-09-13 01:17:31 +0900

    めんどうでした

--- 36.go
+++ 36.go
@@ -1,10 +1,18 @@
 package main
 
-import "http://cdn1.www.st-hatena.com/users/sh/shigemk2/profile.gif"
+import "code.google.com/p/go-tour/pic"
 
-func Pic(dx, dy, int) [][]unit8 {
+func Pic(dx, dy int) [][]uint8 {
+	mat := make([][]uint8, dx)
+	for i := range mat {
+		mat[i] = make([]uint8, dy)
+		for j := range mat[i] {
+			mat[i][j] = uint8(i ^ j)
+		}
+	}
+	return mat
 }
 
 func main() {
-	pic.Show(pic)
+	pic.Show(Pic)
 }
