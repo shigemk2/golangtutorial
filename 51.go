96628729eb32e591c5383bf1c7af558c20c469ad
shigemk2 <i.am.shige@gmail.com>
2014-09-22 23:02:45 +0900

    Add 51

---
+++ 51.go
@@ -0,0 +1,22 @@
+// structだけではなく、パッケージ内で任意の型にもメソッドを定義できます。
+// しかし、他のパッケージからの型や基本型にはメソッドを定義できません。
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func main() {
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+}
