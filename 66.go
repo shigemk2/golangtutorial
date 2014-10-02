ee5b181b647e0f5568ae3c683b964710a6779443
shigemk2 <i.am.shige@gmail.com>
2014-10-02 21:52:08 +0900

    Add 66

---
+++ 66.go
@@ -0,0 +1,23 @@
+// 送り手は、これ以上の送信する値がないことを示すため、チャネルを close できます
+package main
+
+import (
+	"fmt"
+)
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
+func main() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
